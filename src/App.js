3fb567eb3181cfc0959892b01403b1c872c3d192
Alexa Lewis <alexalewisdevelops@gmail.com>
2020-03-23 10:06:06 -0400

    explorer mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
+class App extends Component {
+  state = {
+    diceResult: 0,
+  }
+  
+  rollDice = () => {
+    const result = Math.ceil((Math.random() * 6),
+    console.log('Rolling dice', result),
+    this.setState({
+      diceResult: result,
+    })
+    console.log(this.state.diceResult)
+  } 
+
+  render() {
+    return (
+      <>
+        <section>
+          <button onClick={this.rollDice}>Roll Dice</button>
+        </section>
+        <section>
+          <p>You rolled a ({this.state.diceResult})</p>
+        </section>
+      </>
+    )
+  }
 }
 
 export default App
